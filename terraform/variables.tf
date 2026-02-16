bd0e464fe89378f10827237248af5e180e970ec0
Sena <joaosenadainese@gmail.com>
2026-02-16 12:16:59 -0300

    Ajuste cd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -92,7 +92,7 @@ variable "max_allocated_storage" {
 variable "terraform_state_bucket" {
   description = "Nome do bucket S3 onde está o state da infraestrutura"
   type        = string
-  default     = "fiap-12soat-fase3-joao-dainese"
+  default     = "fiap-12soat-fase4-joao-dainese"
 }
 
 variable "infra_terraform_state_key" {
